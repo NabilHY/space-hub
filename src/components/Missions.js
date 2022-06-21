b29895d6f487ef096b0b7caeb3238b7f6ae60665
Pedro Marques <ppmarq1@gmail.com>
2022-06-21 14:39:05 +0100

    Create basic structure for Missions - Setup

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,8 +1,41 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMissions } from '../redux/missions/missions';
+import './missions.css';
 
 function Missions() {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missionsReducer);
+  useEffect(() => {
+    dispatch(getMissions());
+  }, [dispatch]);
+
   return (
-    <div>Missions</div>
+    <div className="Missions">
+      <table className="Missions__table">
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Join</th>
+          </tr>
+        </thead>
+        {
+          missions.map(({
+            mission_id, mission_name, description, reserved = false,
+          }) => (
+            <tr key={mission_id}>
+              <td className="Missions__name"><h1>{ mission_name }</h1></td>
+              <td className="Missions__description"><p>{ description }</p></td>
+              <td>{ reserved ? (<span className="badge badge--primary">ACTIVE MEMBER</span>) : (<span className="badge badge--secondary">NOT A MEMBER</span>) }</td>
+              <td>{ reserved ? (<button type="button" className="app-btn-danger">Leave Mission</button>) : (<button type="button" className="app-btn-ghost">Join Mission</button>) }</td>
+            </tr>
+          ))
+        }
+      </table>
+    </div>
   );
 }
 
