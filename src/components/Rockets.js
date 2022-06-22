df3eb94a298bac269ca5c35c5d9bada914254688
nabsternab <hayounn.etd@gmail.com>
2022-06-22 10:46:43 +0100

    Render API data

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,8 +1,30 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRockets } from '../redux/rockets/rockets';
 
 function Rockets() {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rocketsReducer);
+  useEffect(() => {
+    dispatch(getRockets());
+  }, [dispatch]);
+
   return (
-    <div>Rockets</div>
+    <div className="rocket-container">
+      {
+        rockets.map(({
+          rocket_id, rocket_name, description, flickr_images,
+        }) => (
+          <div key={rocket_id}>
+            <img src={flickr_images} alt={rocket_name} />
+            <h1>{rocket_name}</h1>
+            <p>{description}</p>
+            <button type="button">Reserve Rocket</button>
+          </div>
+        ))
+      }
+    </div>
   );
 }
 
