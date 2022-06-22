aaced31e275dd85ed4c75582bef1b5b5646a2241
nabsternab <hayounn.etd@gmail.com>
2022-06-22 09:04:21 +0100

    Add code for fetching data and aiton reducer

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,22 +1,30 @@
-import fetchRockets from '../../apiServices/getRocketsApi';
+import fetchRockets from '../../apiServices/getRockets';
 
 const ROCKETS = 'redux/actions/get_rockets';
 
-const rockets = (rockets) => ({
+const rockets = (payload) => ({
   type: ROCKETS,
-  payload: rockets,
+  payload,
 });
 
-export const getRockets = () => (dispach) => {
-  fetchRockets().then((rocket) => {
-    dispach(rockets(rocket));
+export const getRockets = () => async (dispatch) => {
+  const data = await fetchRockets();
+  data.forEach((rocket) => {
+    data.push({
+      id: rocket.id,
+      rocket_name: rocket.rocket_name,
+      description: rocket.description,
+      flickr_images: rocket.flickr_images[0],
+      reserved: false,
+    });
   });
+  dispatch(fetchRockets(rockets));
 };
 
 const rocketsReducer = (state = [], action) => {
   switch (action.type) {
     case ROCKETS:
-      return action.payload;
+      return [...state, action.payload];
     default:
       return state;
   }
