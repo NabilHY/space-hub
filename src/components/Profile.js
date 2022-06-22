2bbb42254371b4d44f678161187ae2a2c3991611
Pedro Marques <ppmarq1@gmail.com>
2022-06-22 11:17:57 +0100

    Add My Profile section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
+import Component from './Component';
+import './profile.css';
 
-function Profile() {
-  return (
-    <div>Profile</div>
+const Profile = () => {
+  const missions = useSelector((state) => state.missionsReducer);
+  const activeMissions = missions.filter(
+    (mission) => mission.reserved === true,
   );
-}
+
+  return (
+    <div className="Reservations">
+      <Component reservations={activeMissions} object="Missions" />
+    </div>
+  );
+};
 
 export default Profile;
